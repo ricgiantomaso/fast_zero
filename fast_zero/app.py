c3bb43d13388c23e917366f44f75d13608c3a405
Ricardo Giantomaso <ricardo_tech@live.com>
2023-07-31 10:41:31 -0300

    ADD/ Schemas for user create and user return Add post route for create users and user base models to insert and reading data.

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fast_zero.schemas import UserSchema, UserPublic
 
 app = FastAPI()
 
@@ -6,3 +7,7 @@ app = FastAPI()
 @app.get('/')
 def read_root():
     return {'message': 'ola mundo'}
+
+@app.post('/users/', status_code = 201, response_model=UserPublic)
+def create_user(user: UserSchema):
+    return UserPublic(**user.model_dump())
