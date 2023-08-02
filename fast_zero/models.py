80c8dec327e718d42259d38896cbbbb25ef6fefa
Ricardo Giantomaso <ricardo_tech@live.com>
2023-08-02 08:55:27 -0300

    ADD/ created database test settings Created database test settings with a conftest file, models, install SQLAlchemy ORM and create .env file

---
+++ fast_zero/models.py
@@ -0,0 +1,13 @@
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+class User(Base):
+    __tablename__ = 'users'
+    id: Mapped[int] = mapped_column(primary_key=True)
+    username: Mapped[str]
+    password: Mapped[str]
+    email: Mapped[str]
