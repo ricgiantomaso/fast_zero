6dbf7d76b416ecec3436e4e496260cf945ab8722
Ricardo Giantomaso <ricardo_tech@live.com>
2023-08-15 08:50:48 -0300

    ADD/ Oauth authentication in endpoints put delete Added oauth authentication in endpoints put and delete. Created tests, token schema and refactoring endpoints put and delete

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,10 +1,17 @@
 from fastapi import Depends, FastAPI, HTTPException
+from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy import select
 from sqlalchemy.orm import Session
 
 from fast_zero.database import get_session
 from fast_zero.models import User
-from fast_zero.schemas import Message, UserList, UserPublic, UserSchema
+from fast_zero.schemas import Message, Token, UserList, UserPublic, UserSchema
+from fast_zero.security import (
+    create_access_token,
+    get_current_user,
+    get_password_hash,
+    verify_password,
+)
 
 app = FastAPI()
 
@@ -17,15 +24,18 @@ def read_root():
 @app.post('/users/', status_code=201, response_model=UserPublic)
 def create_user(user: UserSchema, session: Session = Depends(get_session)):
     db_user = session.scalar(
-        select(User).where(User.username == user.username)
+        select(User).where(User.email == user.email)
     )  ## pesquisa se existe o usuário
     if db_user:
         raise HTTPException(
             status_code=400,
             detail='Username already registered',  ## caso db_user retorne algo
         )
+
+    hashed_password = get_password_hash(user.password)
+
     db_user = User(  ## senão existir, cria um novo usuário
-        username=user.username, password=user.password, email=user.email
+        username=user.username, password=hashed_password, email=user.email
     )
     session.add(db_user)
     session.commit()
@@ -43,26 +53,51 @@ def read_users(
 
 @app.put('/users/{user_id}', response_model=UserPublic)
 def update_user(
-    user_id: int, user: UserSchema, session: Session = Depends(get_session)
+    user_id: int,
+    user: UserSchema,
+    session: Session = Depends(get_session),
+    current_user: User = Depends(get_current_user),
 ):
-    db_user = session.scalar(select(User).where(User.id == user_id))
-    if db_user is None:
-        raise HTTPException(status_code=404, detail='User not found')
-    db_user.username = user.username
-    db_user.password = user.password
-    db_user.email = user.email
-    session.commit()
-    session.refresh(db_user)
+    if current_user.id != user_id:
+        raise HTTPException(status_code=400, detail='Not enough permissions')
 
-    return db_user
+    current_user.username = user.username
+    current_user.password = user.password
+    current_user.email = user.email
+    session.commit()
+    session.refresh(current_user)
+
+    return current_user
 
 
 @app.delete('/users/{user_id}', response_model=Message)
-def delete_user(user_id: int, session: Session = Depends(get_session)):
-    db_user = session.scalar(select(User).where(User.id == user_id))
-    if db_user is None:
-        raise HTTPException(status_code=404, detail='User not found')
-    session.delete(db_user)
+def delete_user(
+    user_id: int,
+    session: Session = Depends(get_session),
+    current_user: User = Depends(get_current_user),
+):
+    if current_user.id != user_id:
+        raise HTTPException(status_code=400, detail='Not enough permissions')
+    session.delete(current_user)
     session.commit()
 
     return {'detail': 'User deleted'}
+
+
+@app.post('/token', response_model=Token)
+def login_for_access_token(
+    form_data: OAuth2PasswordRequestForm = Depends(),
+    session: Session = Depends(get_session),
+):
+    user = session.scalar(select(User).where(User.email == form_data.username))
+    if not user:
+        raise HTTPException(
+            status_code=400, detail='Incorrect email or password'
+        )
+    if not verify_password(form_data.password, user.password):
+        raise HTTPException(
+            status_code=400, detail='Incorrect email or password'
+        )
+    access_token = create_access_token(data={'sub': user.email})
+
+    return {'access_token': access_token, 'token_type': 'bearer'}
